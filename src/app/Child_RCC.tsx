fcb2993453889443e81bf5e400e7537a3b706f93
Ignacio Bonelli <ignacio@nachodigital.com.ar>
2024-01-17 14:20:09 -0300

    Adding some basic behavior to the child component.

--- src/app/Child_RCC.tsx
+++ src/app/Child_RCC.tsx
@@ -1,17 +1,29 @@
 "use client"
 
-import React from 'react';
+import React, {useState} from 'react';
 
 const Child_RCC = (data: any) => {
   const myList: any = data.list;
+  const [state, setState] = useState<boolean>(false);
+
+  const handleButtonClick = () => {
+    if(state == true) {
+      setState(false);
+    } else {
+      setState(true);
+    }
+  };
+
   return (
 		<>
       <h1>Child RCC</h1>
-			<ul>
+			<ul className='list' hidden={state}>
 				{myList.map((item: any) => (
 					<li key={item.name}>{item.name} - {item.age}</li>
 				))}
 			</ul>
+      <br/>
+      <button className='button' onClick={handleButtonClick}>Click To Hide Items</button>
 		</>
   );
 };
